b5b93db0e0525266ed15a6d4f409e7161a73a863
Bhakt Chotaliya <bhaktc19@gmail.com>
2024-02-19 11:31:41 +0530

    code push

--- resources/views/visitors/membershipPlan.blade.php
+++ resources/views/visitors/membershipPlan.blade.php
@@ -38,62 +38,69 @@
     </div>
 
     <div class="container mt-5 mb-5">
-        <div class="row mb-6">
-            @foreach ($adminpackage as $adminpackage)
-            <div class="col-md-4 mt-2">
-                <div class="card membercard shadow p-3 mb-5 bg-body rounded">
-                    <div class="card-body text-center">
-                        <h3 class="card-title text-center" id="adminpackage-title">{{ $adminpackage->title }}
-                            <hr>
-                        </h3>
-                        <h4 class="card-title text-center" id="adminpackage-price">
-                            @if ($adminpackage->price > 0)
-                            <i class='fa fa-rupee'></i>{{ $adminpackage->price }}
-                            @else
-                            <p>-</p>
-                            @endif
-                        </h4>
-                        <p class="card-text mt-5">{!! $adminpackage->details !!}</p>
+        <div class="row mb-5">
+            <div class="col-md-12">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            {{-- <th>Features</th> --}}
+                            @foreach ($adminpackage as $package)
+                            <th>{{ $package->title }}</th>
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {{-- <td>
 
-                        @if (isset(Auth::user()->id))
-                        @if ($adminpackage->title != 'Free')
-                        <div class="card-body text-center">
-                            <form action="{{ route('razorpay.payment.store') }}" method="POST">
-                                @csrf
-                                {{-- <button type="submit" class="btn btn-success mb-5 mt-5">Apply Now</button> --}}
-                                <input type="hidden" name="package" value="{{ $adminpackage->title }}">
-                                <script src="https://checkout.razorpay.com/v1/checkout.js"
-                                    data-key="{{ env('RAZORPAY_KEY') }}" data-amount="{{ $adminpackage->price * 100 }}"
-                                    data-buttontext="Apply Now" data-name="ConsultantCube.com"
-                                    data-description="Rozerpay"
-                                    data-image="{{ url('/visitors/images/ConsultantLogo.jpg') }}"
-                                    data-prefill.name="name" data-theme.color="#333692">
-                                </script>
-                            </form>
-                            <form action="{{ route('free.trial') }}" method="POST">
-                                @csrf
-                                <input type="hidden" name="package" value="{{ $adminpackage->title }} Free Trial">
-                                <button type="submit" class="btn btn-success mb-5 mt-5">Free Trial</button>
-                            </form>
-                        </div>
-                        @endif
-                        @else
-                        <a data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                            class="btn btn-primary mb-5 mt-5">Apply
-                            Now</a>
-                        @endif
-                    </div>
-                </div>
+                                <b>Validity</b> <br> <br>
+                                <b>Online Platform Features</b> <br>
+                                Profile Name <br>
+                                Photo<br>
+                                Description <br>
+                                Profile Creation <br>
+                                Video On Profile Page <br>
+                                Create Workshop on platform online / offline <br>
+                                Publish article <br>
+                                Getting Leads <br>
+                                Option for featured Consultant in his/her category<br> <br> <br>
+
+                                <b>Networking</b> <br>
+                                City level whatsapp group for networking <br>
+                                City level meet up [Discounted Rate as per category] <br>
+                                State level meetup <br>
+                                National level conference <br>
+                                Level based groups <br> <br> <br>
+
+
+                                <b>E-Book</b> <br>
+                                Experience more than 5 years in relevant field <br> <br>
+
+
+                                <b>Support & Resource</b> <br>
+                                Help in Publishing book <br>
+                                Coffee table booklet for corporates <br>
+                                Training Videos <br>
+                                Level up program by Indusrty Expert <br>
+                                Guides for making Youtube channle <br>
+                                Latest Technology related Video Tutorials <br>
+
+                            </td> --}}
+                            @foreach ($adminpackage as $package)
+                            <td>{!! $package->details !!}</td>
+                            @endforeach
+
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            @endforeach
         </div>
     </div>
-
 
     <div class="container allplan">
         <div class="text-center mt-5">
             <h1>All Plans Include</h1>
-            <h5>Bring your people together under your brand on your terms !</h5>
+            <h5>Bring your people together under your brand on your terms!</h5>
         </div>
         <div class="row cardgap gap-lg-4 mt-lg-5 ms-lg-5 flex-wrap">
             <div class="col-md-2 card allplancard shadow p-lg-3 mb-5 bg-gray rounded mt-5" id="allpanlcard-with">
@@ -108,7 +115,7 @@
             <!-- Repeat the pattern for the other plans -->
         </div>
         <div class="d-flex justify-content-center">
-            <a href="#" class="btn btn-primary text-center mt-lg-5 mb-lg-5">Apply Now</a>
+            <a href="#" class="btn text-white mt-lg-5 mb-lg-5">Apply Now</a>
         </div>
     </div>
 
